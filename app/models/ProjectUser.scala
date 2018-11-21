e7d179b8381dd93cf3e26ab424da19a22956a042
yokoyang0001 <y.yokoyang0001@gmail.com>
2018-11-22 05:08:18 +0900

    DRW-15: Add Project table

---
+++ app/models/ProjectUser.scala
@@ -0,0 +1,41 @@
+package models
+
+import java.util._
+
+import dao.Dao
+import io.ebean.annotation.{CreatedTimestamp, UpdatedTimestamp}
+import javax.persistence._
+
+import scala.beans.BeanProperty
+
+@Entity
+@Table(name = "project_users")
+class ProjectUser {
+  @Id
+  var id: Long = _
+
+  @JoinColumn(name = "user_id", insertable = false, updatable = false)
+  @ManyToOne
+  @BeanProperty
+  var user: User = _
+
+  @MapsId("projectId")
+  @JoinColumn(name="project_id", insertable = false, updatable = false)
+  @ManyToOne
+  @BeanProperty
+  var project: Project = _
+
+  @Column(nullable = false, length = 100)
+  @BeanProperty
+  var role: Int = _
+
+  @Column(nullable = false)
+  @CreatedTimestamp
+  var createdAt = new Date
+
+  @Column(nullable = false)
+  @UpdatedTimestamp
+  var updatedAt = new Date
+}
+
+object ProjectUser extends Dao(classOf[ProjectUser]) {}
