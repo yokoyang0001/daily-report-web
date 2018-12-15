702f6ba9021b507d5a29c5d0b28e129a68f3438d
yokoyang0001 <y.yokoyang0001@gmail.com>
2018-12-15 18:14:11 +0900

    API: Delete unnecessary files

---
+++ app/controllers/ApiController.scala
@@ -0,0 +1,16 @@
+package controllers
+
+import javax.inject.{Inject, Singleton}
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+@Singleton
+class ApiController @Inject()(cc: ControllerComponents)
+  extends AbstractController(cc) {
+
+  // Create a simple 'ping' endpoint for now, so that we
+  // can get up and running with a basic implementation
+  def ping = Action { implicit request =>
+    Ok("Hello, Scala!")
+  }
+
+}
